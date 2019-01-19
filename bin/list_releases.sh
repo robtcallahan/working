5f9701cde4067da693e10a0a0b35d0a448e48d99
Rob Callahan <rcallahan@idirect.net>
2019-01-19 15:36:03 -0500

    updated list_releases.sh to use the new s3 bucket

--- bin/list_releases.sh
+++ bin/list_releases.sh
@@ -3,8 +3,9 @@
 #aws s3 ls s3://idirect-releases --profile idirect-enterprise | sed -e 's/^\s\+//' | sed -e 's/\/$//'
 
 profile="--profile idirect-enterprise"
-s3bucket="idirect-releases"
-domain="https://d8lkzio2al7h7.cloudfront.net"
+s3bucket="idirect-enterprise-prod-releases-origin"
+#domain="https://d8lkzio2al7h7.cloudfront.net"
+domain="https://d1uhtjulqigxza.cloudfront.net"
 
 aws s3 ls s3://$s3bucket --recursive $profile | awk '{$1="";$2="";$3=""; print $0}' | sed -e 's/^\s\+//' | while read -r file
 do
